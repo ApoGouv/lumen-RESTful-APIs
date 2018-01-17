609472bce875e1dd8418aa7373625c5b50670ef7
Apostolos Gouvalas <apo.gouv@gmail.com>
2018-01-17 17:06:49 +0200

    Implement post in complex xontrollers

--- LumenAPI/app/Http/Controllers/CourseStudentController.php
+++ LumenAPI/app/Http/Controllers/CourseStudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Course;
+use App\Student;
 
 class CourseStudentController extends Controller
 {
@@ -29,8 +30,32 @@ class CourseStudentController extends Controller
         return $this->createErrorResponse('We couldn\'t find a course with the given id.',404);
     }
 
-    public function store(){
-        return __METHOD__;
+    public function store($course_id, $student_id){
+
+        $course = Course::find($course_id);
+
+        // check if course exists
+        if ($course) {
+            $student = Student::find($student_id);
+
+            // check if student exists
+            if ($student) {
+
+                // check if student already is IN this course
+                if ($course->students()->find($student->id)) {
+                    return $this->createErrorResponse("The student with id {$student->id} already exists in the course with id {$course->id}", 409);
+                }
+                // add student to the students list for this course
+                $course->students()->attach($student->id);
+
+                return $this->createSuccessResponse("The student with id {$student_id}, was added to the course with id {$course_id}",200);
+            }//End if student
+
+            return $this->createErrorResponse("The student with id {$student_id}, does not exist.", 404);
+
+        }//End if course
+
+        return $this->createErrorResponse("The course with id {$course_id}, does not exist.", 404);
     }
 
     public function destroy(){
